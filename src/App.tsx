b0d04b13ad396dc2c738b610a079072f1df74b7f
username <drewesk@gmail.com>
2025-08-11 15:42:55 -0400

    first components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Button, HStack, Heading, Link } from "@chakra-ui/react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <Heading
+        as="h1"
+        size={{ base: "2xl", md: "3xl", lg: "4xl" }}
+        lineHeight="1.1"
+        fontWeight="extrabold"
+        letterSpacing="-0.02em"
+      >
+        Xplastic.io
+      </Heading>
+      <h4>Rid your life of harmful micro-plastics 🔪</h4>
+      <HStack>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+        </Button>
+        <Button
+          as={Link}
+          href="https://dontdie.bryanjohnson.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Don’t Die
+        </Button>
+
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
